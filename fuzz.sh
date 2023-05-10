c76afc3298ed940f7aee1010d0e2e94178994131
Alex <130258074+alex-h4cker@users.noreply.github.com>
2023-05-10 16:22:30 -0400

    Update fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-while read website;
-do echo "Testing $website"
-curl -l $website | grep HTTP/1.1 | awk {'print $2'}
-done < websites.txt
+
+urls_file="urls.txt"
+
+
+while read url; do
+
+  response=$(curl -s -I "$url")
+
+  status_code=$(echo "$response" | grep "HTTP" | awk '{print $2}')
+  content_length=$(echo "$response" | grep "Content-Length" | awk '{print $2}')
+
+  echo "URL: $url"
+  echo "Status Code: $status_code"
+  echo "Content Length: $content_length"
+done < "$urls_file"
